aa0455831d895b1250c5bcdb3c27ac3ccfe79b28
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-02-13 12:57:51 +0700

    display data question dan pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+defineProps({
+    meta: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <nav class="mt-4" aria-label="Pagination">
+        <ul class="pagination pagination-sm justify-content-center">
+            <li class="page-item " v-for="(link, index) in meta.links" :key="index"
+                :class="{ active: link.active, disabled: !link.url }">
+                <Link class="page-link" :href="link.url" preserve-scroll>{{ link.label }}</Link>
+            </li>
+        </ul>
+    </nav>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return QuestionResource::collection(
+    $questions = QuestionResource::collection(
         Question::with('user')->latest()->paginate(15)
     );
 
-    // return Inertia('Questions/Index', [
-    //     'questions' => [
-    //         ['id' => 1, 'title' => 'Question 1'],
-    //         ['id' => 2, 'title' => 'Question 2'],
-    //     ]
-    // ]);
+    return $questions;
+
+    return Inertia('Questions/Index', [
+        'questions' => $questions
+    ]);
 })->name('questions.index');
 
 Route::get('/question/{id}', function ($id) {
